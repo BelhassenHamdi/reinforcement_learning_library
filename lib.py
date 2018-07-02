fdc1f627f265ed771122b2754d085e00a73cfd9a
Hamdi Belhassen <hamdi.belhassen@ensi-uma.tn>
2018-07-03 01:42:43 +0200

    epsilon greedy algorithm for action choice

--- lib.py
+++ lib.py
@@ -4,17 +4,29 @@ methods and related functions.
 '''
 
 import operator
+import numpy as np
 
 
 #  test set for functions evaluation those function way or not undergo some modification
 #  depending on the input and output structure that we would evaluate as most suitable for 
 #  large upcoming usage
 num_iteration = 152
+epsilon1 = 0.05 # I named it epsilon 1 cause epsilon is a token in python
 action_value_state = {'a':1000, 'b':3000, 'c': 100, 'f': 321}
 reward = {}
 
 def greedy(action_value_dict):
     return max(action_value_dict.items(), key=operator.itemgetter(1))[0]
+
+def epsilonGreedy(action_value_dict, epsy):
+    action_type = np.random.choice(['exploitation', 'exploration'], p=[1-epsy, epsy])
+    if action_type == 'exploitation':
+        return max(action_value_dict.items(), key=operator.itemgetter(1))[0]
+    else :
+        expoitation_action = max(action_value_dict.items(), key=operator.itemgetter(1))[0]
+        exploration_action_space = list(action_value_dict.keys())
+        exploration_action_space.remove(expoitation_action)
+        return np.random.choice(exploration_action_space)
 
 def ActionValueUpdateComputation(action_value_dict, action, iteration, reward):
     old =action_value_dict[action]
@@ -23,4 +35,5 @@ def ActionValueUpdateComputation(action_value_dict, action, iteration, reward):
 
 
 print(greedy(action_value_state))
+print(epsilonGreedy(action_value_state, epsilon1))
 print(ActionValueUpdateComputation(action_value_state, 'c', 123, 3))
